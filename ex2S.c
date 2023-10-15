ea179c917eae2505a5e229ad15fc5549b5a3c50d
marinamai <142596833+marinamai@users.noreply.github.com>
2023-10-15 10:08:57 -0300

    ex2S.c

--- ex2S.c
+++ ex2S.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-/*2. Uma unidade de área freqüentemente utilizada para expressar áreas de terra é o hectare, definido como 10^4 m2. Uma mina de carvão a céu aberto consome 75 hectares de terra, a uma profundidade
-de 26 m por ano. Calcule o volume de terra retirada neste tempo em km3
-.*/
 
 int main(){
 
